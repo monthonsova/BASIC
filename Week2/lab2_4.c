5934814a9fa9d41a5980622dd23927513c20878e
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 01:15:56 +0700

    emo

--- Week2/lab2_4.c
+++ Week2/lab2_4.c
@@ -1,9 +1,44 @@
-// Week 2 - Loop Statement
-// Lab 2.4
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 2 - Lab 2.4\n");
+
+    float startingDebt, monthlyPay, monthlyInterest;
+    int monthTracker = 0;
+
+    const float INT_RATE = 0.01f;
+    const float FIXED_PENALTY = 10.0f;
+
+    if (scanf("%f %f", &startingDebt, &monthlyPay) != 2) {
+        return 1;
+    }
+
+    float currentDebt = startingDebt;
+
+    while (currentDebt > 0) {
+
+        monthTracker++;
+
+        monthlyInterest = currentDebt * INT_RATE;
+        currentDebt += monthlyInterest;
+
+        if (monthlyPay < monthlyInterest) {
+            currentDebt += FIXED_PENALTY;
+        }
+
+        currentDebt -= monthlyPay;
+
+        printf("Month %d: Remaining %.2f\n", monthTracker, currentDebt);
+
+        if (monthTracker >= 10 && currentDebt > 0 && monthlyPay < monthlyInterest) {
+            break;
+        }
+    }
+
+    if (currentDebt <= 0) {
+        printf("Loan settled in %d months.\n", monthTracker);
+    } else {
+        printf("Loan settled in 10+ months.\n");
+    }
+
     return 0;
 }
--- Week2/lab2_5.c
+++ Week2/lab2_5.c
@@ -1,9 +1,32 @@
-// Week 2 - Loop Statement
-// Lab 2.5
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 2 - Lab 2.5\n");
+
+    int score;
+    int gradeA = 0, gradeB = 0, gradeC = 0, gradeD = 0, gradeF = 0;
+
+    if (scanf("%d", &score) != 1) {
+        return 1;
+    }
+
+    while (score != -1) {
+
+        if (score >= 80) gradeA++;
+        else if (score >= 70) gradeB++;
+        else if (score >= 60) gradeC++;
+        else if (score >= 50) gradeD++;
+        else gradeF++;
+
+        if (scanf("%d", &score) != 1) {
+            break;
+        }
+    }
+
+    printf("Grade A Count: %d\n", gradeA);
+    printf("Grade B Count: %d\n", gradeB);
+    printf("Grade C Count: %d\n", gradeC);
+    printf("Grade D Count: %d\n", gradeD);
+    printf("Grade F Count: %d\n", gradeF);
+
     return 0;
 }
