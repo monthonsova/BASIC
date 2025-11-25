15a1aa91ca71eefc13b642645663791ea298d3f9
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 01:36:42 +0700

    Update lab3_3.c

--- Week3/lab3_3.c
+++ Week3/lab3_3.c
@@ -1,9 +1,38 @@
-// Week 3 - Structure
-// Lab 3.3
-
 #include <stdio.h>
 
+struct Student {
+    char name[50];
+    int studentId;
+    float score;
+};
+
 int main() {
-    printf("Week 3 - Lab 3.3\n");
+
+    int totalEntries;
+    if (scanf("%d", &totalEntries) != 1) {
+        return 1;
+    }
+
+    struct Student people[totalEntries];
+
+    float pooledScore = 0.0;
+
+    for (int i = 0; i < totalEntries; i++) {
+        if (scanf("%d %f %s",
+            &people[i].studentId,
+            &people[i].score,
+            people[i].name) != 3) {
+            return 1;
+        }
+        pooledScore += people[i].score;
+    }
+
+    float avgScore = 0.0;
+    if (totalEntries > 0) {
+        avgScore = pooledScore / totalEntries;
+    }
+
+    printf("Average Score: %.2f\n", avgScore);
+
     return 0;
 }
