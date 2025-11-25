24052ac8cea44392ce215ef3df180e63af26aff8
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 00:59:27 +0700

    Update lab1_8.c

--- Week1/lab1_8.c
+++ Week1/lab1_8.c
@@ -1,9 +1,28 @@
-// Week 1 - Control Flow & Decisions
-// Lab 1.8
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 1 - Lab 1.8\n");
+
+    int dayCode, hour;
+
+    if (scanf("%d %d", &dayCode, &hour) != 2) {
+        return 1;
+    }
+
+    switch (dayCode) {
+        case 1: case 2: case 3: case 4: case 5:
+            if (hour >= 8 && hour <= 17)
+                printf("System Running (Workday)\n");
+            else
+                printf("System Idle (Off-hours)\n");
+            break;
+
+        case 6: case 7:
+            printf("Weekend Relax Mode\n");
+            break;
+
+        default:
+            printf("Invalid Day Code\n");
+    }
+
     return 0;
 }
