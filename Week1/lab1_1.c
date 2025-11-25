4f47a3c1b52be1c17c84d06c61139e87fe2e1e8a
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 00:34:55 +0700

    Update lab1_1.c

--- Week1/lab1_1.c
+++ Week1/lab1_1.c
@@ -1,9 +1,21 @@
-// Week 1 - Control Flow & Decisions
-// Lab 1.1
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 1 - Lab 1.1\n");
+
+    int loopCount;
+    
+    if (scanf("%d", &loopCount) != 1) {
+        return 1;
+    }
+
+    if (loopCount <= 0) {
+        printf("Error\n");
+        return 0;
+    }
+
+    for (int rounder = 0; rounder < loopCount; rounder++) {
+        printf("Hello Loop!\n");
+    }
+
     return 0;
 }
