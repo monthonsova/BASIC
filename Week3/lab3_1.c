e500654c2fdae4668c1dee752bb6a358ac036e74
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 01:27:25 +0700

    Update lab3_1.c

--- Week3/lab3_1.c
+++ Week3/lab3_1.c
@@ -1,9 +1,21 @@
-// Week 3 - Structure
-// Lab 3.1
-
 #include <stdio.h>
 
+struct Product {
+    char name[50];
+    int productId;
+    float price;
+};
+
 int main() {
-    printf("Week 3 - Lab 3.1\n");
+
+    struct Product shelfItem;
+
+    if (scanf("%d %f %s", &shelfItem.productId, &shelfItem.price, shelfItem.name) != 3) {
+        return 1;
+    }
+
+    printf("ID: %d, Price: %.2f, Name: %s\n",
+           shelfItem.productId, shelfItem.price, shelfItem.name);
+
     return 0;
 }
