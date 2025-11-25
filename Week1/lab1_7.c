115c5a567de7196b934a121687df8a8c17d32aba
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 00:58:08 +0700

    Update lab1_7.c

--- Week1/lab1_7.c
+++ Week1/lab1_7.c
@@ -1,9 +1,23 @@
-// Week 1 - Control Flow & Decisions
-// Lab 1.7
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 1 - Lab 1.7\n");
+
+    int type;
+    float kwh, bill = 0.0;
+
+    if (scanf("%d %f", &type, &kwh) != 2) return 1;
+
+    if (type == 1) {
+        bill = (kwh <= 100 ? kwh * 3.0 : kwh * 4.0);
+        printf("%.2f\n", bill);
+    }
+    else if (type == 2) {
+        bill = (kwh <= 500 ? kwh * 5.0 : kwh * 6.5);
+        printf("%.2f\n", bill);
+    }
+    else {
+        printf("Invalid Customer Type\n");
+    }
+
     return 0;
 }
