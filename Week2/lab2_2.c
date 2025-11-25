d2a5bbcf8ce32c1510a8f83814dbf54db771d87f
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 01:12:14 +0700

    Update lab2_2.c

--- Week2/lab2_2.c
+++ Week2/lab2_2.c
@@ -1,9 +1,32 @@
-// Week 2 - Loop Statement
-// Lab 2.2
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 2 - Lab 2.2\n");
+
+    int itemCount;
+    if (scanf("%d", &itemCount) != 1) {
+        return 1;
+    }
+
+    float unitCost, grandTotal = 0.0f;
+    int qty;
+
+    for (int idx = 0; idx < itemCount; idx++) {
+
+        scanf("%f", &unitCost);
+        scanf("%d", &qty);
+
+        float rawPrice = unitCost * qty;
+
+        float discounted = rawPrice;
+
+        if (unitCost >= 1000.0f) {
+            discounted = rawPrice * 0.90f;
+        }
+
+        grandTotal += discounted;
+    }
+
+    printf("Grand Total: %.2f\n", grandTotal);
+
     return 0;
 }
