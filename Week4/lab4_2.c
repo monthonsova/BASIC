de5533b2338313499d3fd762ff27a804a457bdde
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 02:03:02 +0700

    Update lab4_2.c

--- Week4/lab4_2.c
+++ Week4/lab4_2.c
@@ -1,9 +1,20 @@
-// Week 4 - Array
-// Lab 4.2
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 4 - Lab 4.2\n");
+    int total, k;
+    int sumPass = 0;
+    int flop = 0;
+
+    scanf("%d", &total);
+    int pile[total];
+
+    for (k = 0; k < total; k++) {
+        scanf("%d", &pile[k]);
+        if (pile[k] >= 50) sumPass += pile[k];
+        else flop++;
+    }
+
+    printf("Passing Scores Sum: %d\n", sumPass);
+    printf("Failing Students Count: %d\n", flop);
     return 0;
 }
