9558aede7eb6b5fe7026c2ce360a2e128a19ad75
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 02:03:59 +0700

    emo

--- Week4/lab4_3.c
+++ Week4/lab4_3.c
@@ -1,9 +1,22 @@
-// Week 4 - Array
-// Lab 4.3
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 4 - Lab 4.3\n");
+    int dayTemp[7];
+    int apex;
+    int d;
+
+    for (d = 0; d < 7; d++) scanf("%d", &dayTemp[d]);
+
+    apex = dayTemp[0];
+    for (d = 1; d < 7; d++) if (dayTemp[d] > apex) apex = dayTemp[d];
+
+    printf("--- DAILY TEMPERATURE REPORT ---\n");
+    printf("Recorded Temperatures (C): ");
+    for (d = 0; d < 7; d++) printf("%d ", dayTemp[d]);
+    printf("\n");
+    printf("Maximum Temperature Found: %d C\n", apex);
+
+    if (apex >= 35) printf("Weather is HOT!\n");
+    else printf("Weather is MODERATE.\n");
     return 0;
 }
