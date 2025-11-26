6c032de702aa8fac917ac017bfd0cc6514bdb892
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 13:04:44 +0700

    Update lab4_12.c

--- Week4/lab4_12.c
+++ Week4/lab4_12.c
@@ -1,9 +1,54 @@
-// Week 4 - Array
-// Lab 4.12
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 4 - Lab 4.12\n");
+    int A[2][3], B[2][3], BT[3][2], C[2][2];
+    int i, j, k;
+
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 3; j++)
+            scanf("%d", &A[i][j]);
+
+    for (i = 0; i < 2; i++)
+        for (j = 0; j < 3; j++)
+            scanf("%d", &B[i][j]);
+
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 2; j++)
+            BT[i][j] = B[j % 2][i];
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            C[i][j] = 0;
+            for (k = 0; k < 3; k++)
+                C[i][j] += A[i][k] * BT[k][j];
+        }
+    }
+
+    printf("--- MATRIX MULTIPLICATION REPORT ---\n");
+
+    printf("\nMatrix A (2x3):\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++) printf("%4d ", A[i][j]);
+        printf("\n");
+    }
+
+    printf("\nMatrix B (2x3):\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++) printf("%4d ", B[i][j]);
+        printf("\n");
+    }
+
+    printf("\nMatrix B Transposed (BT, 3x2):\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 2; j++) printf("%4d ", BT[i][j]);
+        printf("\n");
+    }
+
+    printf("\nResult Matrix C (A x BT, 2x2):\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) printf("%4d ", C[i][j]);
+        printf("\n");
+    }
+
     return 0;
 }
