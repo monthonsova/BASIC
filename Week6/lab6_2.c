dfa0f19b49027c2e7fcd4a0b38ea0b1dcbe90418
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 13:34:24 +0700

    Update lab6_2.c

--- Week6/lab6_2.c
+++ Week6/lab6_2.c
@@ -1,9 +1,26 @@
-// Week 6 - Basic Pointer
-// Lab 6.2
-
 #include <stdio.h>
 
+void swap_values(int *a, int *b) {
+    int tempBox = *a;
+    *a = *b;
+    *b = tempBox;
+}
+
 int main() {
-    printf("Week 6 - Lab 6.2\n");
+    int leftNum, rightNum;
+
+    printf("Enter value for Num1: ");
+    scanf("%d", &leftNum);
+
+    printf("Enter value for Num2: ");
+    scanf("%d", &rightNum);
+
+    printf("\n--- SWAPPING REPORT ---\n");
+    printf("Before Swap - Num1: %d, Num2: %d\n", leftNum, rightNum);
+
+    swap_values(&leftNum, &rightNum);
+
+    printf("After Swap - Num1: %d, Num2: %d\n", leftNum, rightNum);
+
     return 0;
 }
