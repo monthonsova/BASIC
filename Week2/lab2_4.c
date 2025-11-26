01452a4856ae64d19fce35a74ae205de82afad42
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 15:43:23 +0700

    Update lab2_4.c

--- Week2/lab2_4.c
+++ Week2/lab2_4.c
@@ -2,43 +2,38 @@
 
 int main() {
 
-    float startingDebt, monthlyPay, monthlyInterest;
-    int monthTracker = 0;
+    float currentDebt, monthlyPay, interestGain;
+    int monthTick = 0;
 
-    const float INT_RATE = 0.01f;
-    const float FIXED_PENALTY = 10.0f;
+    const float RATE = 0.01f;
+    const float OVERDUE_FEE = 10.0f;
 
-    if (scanf("%f %f", &startingDebt, &monthlyPay) != 2) {
+    if (scanf("%f %f", &currentDebt, &monthlyPay) != 2) {
         return 1;
     }
 
-    float currentDebt = startingDebt;
+    while (currentDebt > 0.0f) {
 
-    while (currentDebt > 0) {
+        interestGain = currentDebt * RATE;
+        currentDebt += interestGain;
 
-        monthTracker++;
-
-        monthlyInterest = currentDebt * INT_RATE;
-        currentDebt += monthlyInterest;
-
-        if (monthlyPay < monthlyInterest) {
-            currentDebt += FIXED_PENALTY;
+        if (monthlyPay < interestGain) {
+            currentDebt += OVERDUE_FEE;
         }
 
         currentDebt -= monthlyPay;
 
-        printf("Month %d: Remaining %.2f\n", monthTracker, currentDebt);
+        monthTick++;
 
-        if (monthTracker >= 10 && currentDebt > 0 && monthlyPay < monthlyInterest) {
-            break;
+        printf("Month %d: Remaining %.2f\n", monthTick, currentDebt);
+
+        if (monthTick == 10 && currentDebt > 0.0f) {
+            printf("Loan settled in 10+ months.\n");
+            return 0;
         }
     }
 
-    if (currentDebt <= 0) {
-        printf("Loan settled in %d months.\n", monthTracker);
-    } else {
-        printf("Loan settled in 10+ months.\n");
-    }
+    printf("Loan settled in %d months.\n", monthTick);
 
     return 0;
 }
