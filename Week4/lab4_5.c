4efece4a76990ddbbe7d5984e491f04f48c60b67
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 12:57:33 +0700

    emo

--- Week4/lab4_5.c
+++ Week4/lab4_5.c
@@ -1,9 +1,19 @@
-// Week 4 - Array
-// Lab 4.5
-
 #include <stdio.h>
 
 int main() {
-    printf("Week 4 - Lab 4.5\n");
+    int pack[6];
+    int total = 0;
+    int i;
+
+    for (i = 0; i < 6; i++) {
+        scanf("%d", &pack[i]);
+        total += pack[i];
+    }
+
+    printf("--- SUMMATION REPORT ---\n");
+    printf("Recorded Numbers: ");
+    for (i = 0; i < 6; i++) printf("%d ", pack[i]);
+    printf("\nTotal Sum of Numbers: %d\n", total);
+
     return 0;
 }
