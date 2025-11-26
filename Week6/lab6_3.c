131bce3720f19d55633cf3a96f94319b462d132a
Monthon Sova <igaccmonthon1150@gmail.com>
2025-11-26 13:36:54 +0700

    emo

--- Week6/lab6_3.c
+++ Week6/lab6_3.c
@@ -1,9 +1,30 @@
-// Week 6 - Basic Pointer
-// Lab 6.3
-
 #include <stdio.h>
+#define SIZE 3
 
 int main() {
-    printf("Week 6 - Lab 6.3\n");
+    int data[SIZE];
+    int *ptr;
+    int k;
+
+    printf("Enter %d integer elements for the array:\n", SIZE);
+    for (k = 0; k < SIZE; k++) {
+        printf("Element %d: ", k);
+        scanf("%d", &data[k]);
+    }
+
+    ptr = data;
+
+    printf("\n--- ARRAY AND POINTER REPORT ---\n");
+    printf("Index | Value (Direct) | Value (via Pointer) | Address\n");
+    printf("--------------------------------------------------\n");
+
+    for (k = 0; k < SIZE; k++) {
+        printf("%5d | %14d | %19d | %p\n",
+               k,
+               data[k],
+               *(ptr + k),
+               (ptr + k));
+    }
+
     return 0;
 }
--- Week6/lab6_4.c
+++ Week6/lab6_4.c
@@ -1,9 +1,27 @@
-// Week 6 - Basic Pointer
-// Lab 6.4
-
 #include <stdio.h>
 
+void calculate_sum_product(int num1, int num2, int *sum_ptr, int *prod_ptr) {
+    *sum_ptr = num1 + num2;
+    *prod_ptr = num1 * num2;
+}
+
 int main() {
-    printf("Week 6 - Lab 6.4\n");
+    int valA, valB;
+    int resultSum, resultProd;
+
+    printf("Enter first integer (Val A): ");
+    scanf("%d", &valA);
+
+    printf("Enter second integer (Val B): ");
+    scanf("%d", &valB);
+
+    calculate_sum_product(valA, valB, &resultSum, &resultProd);
+
+    printf("\n--- CALCULATION REPORT ---\n");
+    printf("Input Values: A = %d, B = %d\n", valA, valB);
+    printf("----------------------------------\n");
+    printf("Result Sum (A + B): %d\n", resultSum);
+    printf("Result Product (A * B): %d\n", resultProd);
+
     return 0;
 }
